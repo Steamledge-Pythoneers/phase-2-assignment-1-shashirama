a911cb975786244c4e6c8f51544a15615d031d5f
Adebayo Awofuye <38406229+drak00@users.noreply.github.com>
2020-09-29 12:01:13 +0100

    Revert "Shows completed assignment"
    This reverts commit 6b25fe8ae16c9afd8e2a849afb4d860c04e931fb.

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,23 +1,4 @@
 ## TODO: complete the function "lowest_terms" below
 
 def lowest_terms(x):
-	'''x = ["2/1", "4/7", "-2/1", "-1/3", "-20/11", "6/13", "Undefined", "0"]
-	for i in x:
-		i'''
-	if x == "20/10":
-		return "2/1"
-	elif x == "16/28":
-		return "4/7"
-	elif x == "-50/25":
-		return "-2/1"
-	elif x == "20/-60":
-		return "-1/3"
-	elif x == "-300/165":
-		return "-20/11"
-	elif x == "-12/-26":
-		return "6/13"
-	elif x == "90/0":
-		return "Undefined"
-	else:
-		return "0"
-	
+	return ""
