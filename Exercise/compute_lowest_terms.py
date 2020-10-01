daaa5ba7263190abf394ed7a8ac80db7d0c5e0fb
Iro Munir <shashirama545@gmail.com>
2020-10-01 01:40:50 +0100

    final error fixed

--- Exercise/compute_lowest_terms.py
+++ Exercise/compute_lowest_terms.py
@@ -1,10 +1,7 @@
 ## TODO: complete the function "lowest_terms" below
 # HCF finder function
 def Hcf(num1, num2):
-    if num1 and num2 == 0:
-        hcf = 0
-        return hcf
-    elif num1 and num2 < 0:
+    if num1 and num2 < 0:
         smaller = abs(num1)
     elif num1 < 0:
         smaller = num2
@@ -26,22 +23,23 @@ def lowest_terms(x):
     x1, x2 = map(int, x_split)
     if x1 == 0:
         return "0"
-
     elif x2 == 0:
         return "Undefined"
-
     else:
         hcf = Hcf(x1, x2)
         numerator = x1 // hcf
         denominator = x2 // hcf
-        if numerator and denominator < 0:
-            solution1 = "{}/{}".format(abs(numerator), abs(denominator))
-            return solution1
-        else:
-        #answer = (numerator/denominator).as_integer_ratio()
-        #(numerator1, denominator1) = answer
-            solution = "{}/{}".format(numerator, denominator)#str(answer[0]) + "/" + str(answer[1])
+        if denominator < 0:
+            numerator*=-1
+            solution2 = "{}/{}".format((numerator), abs(denominator))
+            return solution2
+        elif numerator and denominator < 0:
+            solution = "{}/{}".format(abs(numerator), abs(denominator))
             return solution
+
+        else:
+            solution1 = "{}/{}".format(numerator, denominator)
+            return solution1
 
 #print(Hcf(0, 0))
 
